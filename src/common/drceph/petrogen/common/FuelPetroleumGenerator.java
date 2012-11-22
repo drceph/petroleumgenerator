2fbfa3549f1bd722eece1bab747c197c749e604e
Chris Duran <chris@chrisduran.co>
2012-11-22 11:56:27 +1100

    Added licence information to repository and source
    GPLv3

--- src/common/drceph/petrogen/common/FuelPetroleumGenerator.java
+++ src/common/drceph/petrogen/common/FuelPetroleumGenerator.java
@@ -1,3 +1,29 @@
+/* Petroleum Generator: a Buildcraft/IndustrialCraft2 crossover mod for Minecraft.
+ *
+ * Version 0.4
+ *
+ * This software is available through the following channels:
+ * http://github.com/chrisduran/petroleumgenerator
+ *
+ * Copyright (c) 2012  Chris Duran
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * https://github.com/chrisduran/petroleumgenerator/blob/master/LICENCE
+ *
+ */
+
 package drceph.petrogen.common;
 
 import java.util.HashMap;
